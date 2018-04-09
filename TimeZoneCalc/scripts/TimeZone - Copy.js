85e7ec5403d182ffc8c355d020a860563ea6d0d3
SibeeshVenu <sibikv4u@gmail.com>
2018-04-09 09:52:44 +0530

    Initial Commit

---
+++ TimeZoneCalc/scripts/TimeZone - Copy.js
@@ -0,0 +1,108 @@
+﻿$(document).ready(function () {
+    $("#txtTimeZone").datepicker();
+    //var zones = moment.tz.names();
+    //for (var i = 0; i < zones.length; i++) {
+    //    $('#dataZones').append('<option value=' + zones[i] + '>' + zones[i] + '</option>');
+    //}
+    $('#btnSubmit').click(function () {
+        var dayLight = new Date(); //Wed Jun 15 2016 14:41:38 GMT+0530 (India Standard Time)
+        dayLight = new Date($('#txtTimeZone').val());
+        //dayLight = dayLight.toLocaleString('en-US', { timeZone: $('#txtTimeZone').val() });
+
+        //var dt = new timezoneJS.Date(dayLight);
+        //dt.setTimezone($('#dataZones').val());
+
+
+        //var a = toTimeZone(moment.utc($('#txtTimeZone').val()).format(), $('#dataZones').val());
+        if (dayLight.dst()) {
+            timeZoneValue = dayLight.stdTimezoneOffset();// Day light time is OFF
+        } else {
+            timeZoneValue = dayLight.stdTimezoneOnset();// Day light time is ON
+        }
+        $("#placeholder").text('The time zone value for ' + dayLight + ' is ' + timeZoneValue);
+    });
+});
+function toTimeZone(time, zone) {
+    var format = 'yy/mm/dd hh:mm:ss';
+    return moment(time).tz(zone);
+}
+Date.prototype.dst = function () {
+    return this.getTimezoneOffset() < this.stdTimezoneOffset();
+}
+Date.prototype.stdTimezoneOffset = function () {
+    var jan = new Date(this.getFullYear(), 0, 1);
+    var jul = new Date(this.getFullYear(), 6, 1);
+    return (Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset()) * -1);
+}
+Date.prototype.stdTimezoneOnset = function () {
+    var jan = new Date(this.getFullYear(), 0, 1);
+    var jul = new Date(this.getFullYear(), 6, 1);
+    return (Math.min(jan.getTimezoneOffset(), jul.getTimezoneOffset()) * -1);
+}
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+        debugger;
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+            $('#latlon').text(place.geometry.location.lat() + ',' + place.geometry.location.lng() + ', The time zone value is ' + place.utc_offset);
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
